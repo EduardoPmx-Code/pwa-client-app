ef03ce7a02f4855c613930495ba4d5b9705b2647
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-04 21:50:58 -0400

    add basic navigation main module

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './main.component';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule
+  ]
+})
+export class MainModule { }

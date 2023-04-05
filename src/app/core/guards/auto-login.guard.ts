24e62528d9fcc7e5bc3cf5a51c603b03e47e1e70
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-04 20:47:55 -0400

    add guards autologin and guard login

---
+++ src/app/core/guards/auto-login.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Router } from '@angular/router';
+import {
+  filter,
+  map,
+  Observable,
+  take,
+} from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AutoLoginGuard implements CanLoad {
+  constructor(private userService: UserService, private router: Router) { }
+
+  canLoad(): Observable<boolean> {
+    return this.userService.isAuthenticated.pipe(
+      filter((val) => val !== null), // Filter out initial Behaviour subject value
+      take(1), // Otherwise the Observable doesn't complete
+      map((isAuthenticated) => {
+        if (isAuthenticated) {
+          // Directly open inside area
+          this.router.navigateByUrl('main', { replaceUrl: true });
+          return false;
+        }
+        return true;
+      }),
+    );
+  }
+}

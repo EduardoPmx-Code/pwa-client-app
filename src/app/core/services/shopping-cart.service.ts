b42267d0d8e024cdd736870a3065323be89cdd40
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-04 20:53:09 -0400

    add cart service

---
+++ src/app/core/services/shopping-cart.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+export interface CartProduct {
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
+  image:string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ShoppingCartService {
+  
+  private cart: CartProduct[] = [];
+  private cartSubject = new BehaviorSubject<CartProduct[]>(this.cart);
+  
+  constructor() { }
+
+  getCart(): Observable<CartProduct[]> {
+    return this.cartSubject.asObservable();
+  }
+
+  addToCart(product: CartProduct): void {
+  /* const existingProduct = this.cart.find(p => p.id === product.id);
+    if (existingProduct) {
+      existingProduct.quantity += product.quantity;
+    } else {
+      this.cart.push(product);
+    }*/
+   this.cart.push(product);
+    this.cartSubject.next(this.cart);
+  }
+
+  removeFromCart(productId:any): void {
+    const index = this.cart.findIndex(p => p.id === productId);
+    if (index >= 0) {
+      this.cart.splice(index, 1);
+      this.cartSubject.next(this.cart);
+    }
+  }
+
+  clearCart(): void {
+    this.cart = [];
+    this.cartSubject.next(this.cart);
+  }
+}
+
+

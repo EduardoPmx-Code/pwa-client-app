93fc582be3728cfbb11d92b4cd22eff244fbccdb
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-04 20:52:34 -0400

    add products  service

---
+++ src/app/core/services/products.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private apiService: ApiService,) { }
+
+  getAllProducts():Observable<any>{
+   return this.apiService.get('/product')
+}
+  searchproducts(params:string):Observable<any>{
+    return this.apiService.get(`/product/search?searchTerm=${params}`)
+  }
+  getProduct(id:string):Observable<any>{
+    return this.apiService.get(`/product/${id}`)
+  }
+  getCategories():Observable<any>{
+    return this.apiService.get(`/categories`) 
+  }
+  getProductsByCategory(categoriId:string){//example
+    return this.apiService.get(`/products:id/products?city=New%20York&state=NY&country=USA&zip=10001`)
+  }
+}

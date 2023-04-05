a2e1762bbf2d0426bbe44560202fd69a5476f9af
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-04 21:48:24 -0400

    add basic navigation app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutoLoginGuard } from './core/guards/auto-login.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule),
+  //  canLoad: [AutoLoginGuard],
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then( m => m.MainModule),
+  //  canLoad: [AutoLoginGuard],
+  },
+  {
+    path: '**', // path empty redirect to login
+    redirectTo: 'auth',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

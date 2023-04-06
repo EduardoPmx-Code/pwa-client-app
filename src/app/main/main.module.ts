11cd408a922e76950c149cadcf278ff196cd9e51
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-06 15:17:17 -0400

    add news page and config taiga

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,9 +5,16 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { TuiLinkModule, TuiLoaderModule } from '@taiga-ui/core';
-import {TuiActionModule, TuiCarouselModule, TuiIslandModule, TuiMarkerIconModule, TuiPaginationModule} from '@taiga-ui/kit';
+import { TUI_SANITIZER, TuiLinkModule, TuiLoaderModule, TuiSvgModule, tuiSvgOptionsProvider } from '@taiga-ui/core';
+import {TuiActionModule, TuiBadgeModule, TuiCarouselModule, TuiInputModule, TuiIslandModule, TuiMarkerIconModule, TuiPaginationModule} from '@taiga-ui/kit';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './order/order.component';
+import { ProductComponent } from './product/product.component';
+import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
 
 @NgModule({
   declarations: [
@@ -15,6 +22,12 @@ import { FooterComponent } from '../shared/components/footer/footer.component';
     NavbarComponent,
     HomeComponent,
     FooterComponent,
+    ProductsListComponent,
+    OrdersComponent,
+    OrderComponent,
+    ProductComponent,
+    ShopingCartComponent,
+    CheckoutComponent,
   ],
   imports: [
     CommonModule,
@@ -26,6 +39,24 @@ import { FooterComponent } from '../shared/components/footer/footer.component';
     TuiIslandModule,
     TuiPaginationModule,
     TuiLinkModule,
+    TuiSvgModule,
+    TuiBadgeModule,
+    TuiInputModule,
+  ],
+  providers:[
+  {
+    provide: TUI_SANITIZER,
+    useClass: NgDompurifySanitizer,
+  },
+  tuiSvgOptionsProvider({
+    srcProcessor: src => {
+        const myCustomPrefix = 'icons8::';
+
+        return String(src).startsWith(myCustomPrefix)
+            ? `assets/icons8/${String(src).replace(myCustomPrefix, '')}.svg`
+            : src;
+    },
+}),
   ]
 })
 export class MainModule { }

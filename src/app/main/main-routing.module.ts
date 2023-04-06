259c1c0d600a10dc3ca7e2866199e2744845bbe4
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-06 10:39:16 -0400

    set route main home

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'',component:MainComponent}
+  {path:'',
+  component:MainComponent,
+  children:[
+    {
+      path:"",
+      redirectTo:"home",
+      pathMatch: 'full'
+    },
+    {
+      path:"home",
+      component:HomeComponent
+    },
+  ]}
 ];
 
 @NgModule({

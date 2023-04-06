2cd66cbf384626e554735acbf5d50985437de1a5
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-06 10:40:50 -0400

    set new config main module add taiga modules and footer

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,16 +4,28 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { NavbarComponent } from '../shared/components/navbar/navbar.component';
-
+import { HomeComponent } from './home/home.component';
+import { TuiLinkModule, TuiLoaderModule } from '@taiga-ui/core';
+import {TuiActionModule, TuiCarouselModule, TuiIslandModule, TuiMarkerIconModule, TuiPaginationModule} from '@taiga-ui/kit';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     MainComponent,
     NavbarComponent,
+    HomeComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    TuiLoaderModule,
+    TuiActionModule,
+    TuiMarkerIconModule,
+    TuiCarouselModule,
+    TuiIslandModule,
+    TuiPaginationModule,
+    TuiLinkModule,
   ]
 })
 export class MainModule { }

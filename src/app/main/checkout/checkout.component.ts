b0195809bc5cf32b768d608f640686f2a1ef5dda
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-07 18:08:24 -0400

    add basic configure call api paypal

--- src/app/main/checkout/checkout.component.ts
+++ src/app/main/checkout/checkout.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+declare var paypal:any;
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-
+  @ViewChild('paypal', { static: true })
+  paypalElement!: ElementRef;
+  product = {
+    price: 777.77,
+    description: 'used couch, decent condition',
+    img: 'assets/couch.jpg'
+  };
+  paidFor = false;
   constructor() { }
 
   ngOnInit(): void {
-  }
-
+    paypal
+    .Buttons({
+      createOrder: (data:any, actions:any) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              description: this.product.description,
+              amount: {
+                currency_code: 'USD',
+                value: this.product.price
+              }
+            }
+          ]
+        });
+      },
+      onApprove: async (data:any, actions:any) => {
+        const order = await actions.order.capture();
+        this.paidFor = true;
+        console.log(order);
+      },
+      onError: (err: any) => {
+        console.log(err);
+      }
+    })
+    .render(this.paypalElement.nativeElement);
+}
 }

b916afb3fc6a39bf77134297230bbcce07af6c6c
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-07 13:49:08 -0400

    add call api product

--- src/app/main/product/product.component.ts
+++ src/app/main/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from 'src/app/core/services/products.service';
+import { ShoppingCartService } from 'src/app/core/services/shopping-cart.service';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  product:any
+  paramsId:any
 
-  constructor() { }
+  constructor(private productsServices:ProductsService, private activateRoute:ActivatedRoute,
+    private cartService:ShoppingCartService, private router:Router,) { }
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe(
+      (data)=>{
+        this.paramsId = data
+        this.productsServices.getProduct(this.paramsId.id).subscribe(
+          (data)=>{
+            console.log(data)
+            this.product = data
+          }
+        )
+      }
+    )
   }
+  addACart(){
+    this.product.quantity =1
+
+    this.cartService.addToCart(this.product)
+    
+    this.router.navigate(["/main/shopping-cart"])
+}
 
 }

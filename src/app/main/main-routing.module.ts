2bcf57a934f6cd9adb38ed07b34871000f300eba
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-11 17:15:30 -0400

    add guart in router checkout

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
     {
       path:"checkout",
       component:CheckoutComponent,
-    //  canActivate: [CheckoutGuard],
+      canActivate: [CheckoutGuard],
     },
   ]}
 ];

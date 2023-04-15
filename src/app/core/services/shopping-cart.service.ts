c99da450fbd6891ab485b9044d63b4b0895d1610
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-15 04:08:13 -0400

    fix cart delete item and sum products

--- src/app/core/services/shopping-cart.service.ts
+++ src/app/core/services/shopping-cart.service.ts
@@ -1,28 +1,37 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+//import { v4 as uuidv4 } from 'uuid';
+
 export interface CartProduct {
-  id: number;
+  _id: string;
+ // id: string; // Agregamos la propiedad id al objeto CartProduct
   title: string;
   price: number;
   quantity: number;
-  image:string;
+  image: string;
 }
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class ShoppingCartService {
   
   private cart: CartProduct[] = [];
   private cartSubject = new BehaviorSubject<CartProduct[]>(this.cart);
-  public cartlength = 0
+  public cartlength = 0;
+  
   constructor() { 
     this.cartSubject.subscribe((data)=>{
-      
-      this.cartlength = data.length
-      console.log(this.cartlength)
-    })
+      this.cartlength = data.length;
+      console.log(this.cartlength);
+    });
+  }
+  getTotalPrice(): number {
+    let totalPrice = 0;
+    for (let i = 0; i < this.cart.length; i++) {
+      totalPrice += this.cart[i].price * this.cart[i].quantity;
+    }
+    return totalPrice;
   }
 
   getCart(): Observable<CartProduct[]> {
@@ -30,29 +39,34 @@ export class ShoppingCartService {
   }
 
   addToCart(product: CartProduct): void {
-  /* const existingProduct = this.cart.find(p => p.id === product.id);
-    if (existingProduct) {
-      existingProduct.quantity += product.quantity;
-    } else {
-      this.cart.push(product);
-    }*/
-   this.cart.push(product);
-    this.cartSubject.next(this.cart);
+   // product.id = uuidv4(); // Generamos un id único para el producto
+   const index = this.cart.findIndex(item => item._id === product._id); // Buscamos el índice del producto correspondiente en el carrito según su id
+
+   if (index === -1) { // Si el producto no existe en el carrito, lo agregamos con cantidad 1
+     product.quantity = 1;
+     this.cart.push(product);
+   } else { // Si el producto ya existe en el carrito, aumentamos su cantidad en 1
+     this.cart[index].quantity += 1;
+   }
+ 
+   this.cartSubject.next(this.cart);
+   this.cartlength = this.cart.length;
   }
 
-  removeFromCart(productId:any): void {
-    const index = this.cart.findIndex(p => p.id === productId);
-    if (index >= 0) {
-      this.cart.splice(index, 1);
+  removeFromCart(productId: string): void {
+    const index = this.cart.findIndex(product => product._id === productId); // Buscamos el índice del producto correspondiente en el carrito según su id
+    if (index !== -1) {
+      this.cart.splice(index, 1); // Eliminamos el producto del carrito
       this.cartSubject.next(this.cart);
-      this.cart.length
+      console.log(this.cart)
+      this.cartlength = this.cart.length; // Actualizamos la longitud del carrito
+    
     }
   }
 
   clearCart(): void {
     this.cart = [];
     this.cartSubject.next(this.cart);
+    this.cartlength = 0;
   }
 }
-
-

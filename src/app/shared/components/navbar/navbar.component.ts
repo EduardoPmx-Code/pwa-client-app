a96cb3792450a1e0e4b0e69da50b7fcca6fd9215
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-14 21:01:42 -0400

    add logOut

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TuiDialogService, TuiSizeL, TuiSizeS } from '@taiga-ui/core';
 import { Subscription } from 'rxjs';
 import { ShoppingCartService } from 'src/app/core/services/shopping-cart.service';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,16 +14,22 @@ import { ShoppingCartService } from 'src/app/core/services/shopping-cart.service
 export class NavbarComponent implements OnInit, OnDestroy {
   cartProducts=0
   search:FormGroup;
-  productList$!:Subscription;
+  productLengt$!:Subscription;
+  dropdownOpen = false;
+  size: TuiSizeL | TuiSizeS = 's';
 
-  constructor(private cart:ShoppingCartService,private fb: FormBuilder, private router:Router) {
+  readonly burgers = ['Classic', 'Cheeseburger', 'Royal Cheeseburger'];
+  readonly drinks = ['Cola', 'Tea', 'Coffee', 'Slurm'];
+
+  constructor(private cart:ShoppingCartService,private fb: FormBuilder, private router:Router,
+    @Inject(TuiDialogService) private readonly dialogs: TuiDialogService,private userService:UserService) {
     this.search= this.fb.group({
       search:['']
     })
    }
 
   ngOnInit(): void {
-    this.cart.getCart().subscribe(
+   this.productLengt$ = this.cart.getCart().subscribe(
       (data=>{
         this.cartProducts = data.length
       })
@@ -29,12 +37,19 @@ export class NavbarComponent implements OnInit, OnDestroy {
     
   }
   searchHandler(){
-    this.router.navigate([`/main/products-list/${ this.search.value.search }`])
+    this.router.navigate([`/main/products-list/search:${ this.search.value.search }`])
     this.search.reset()
   }
+  selectOption(item: string): void {
+    this.dropdownOpen = false;
+    this.dialogs.open(`You selected ${item}`).subscribe();
+}
 
   ngOnDestroy(): void {
-    this.productList$.unsubscribe()
+    this.productLengt$.unsubscribe()
+   }
+   logOut(){
+    this.userService.purgeAuth()
    }
 
 }

02dfa3da06c4f23bb8bcbf5dfe6fb755cf844c62
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-15 04:04:13 -0400

    add service call geolocation navigator

---
+++ src/app/core/services/geolocation.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { resolve } from 'dns';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeolocationService {
+  public userLocation: [number, number] | undefined;
+  get isUserLocationReady():boolean{
+    return !!this.userLocation
+  }
+
+  constructor() { 
+    this.getUserLocation()
+  }
+
+  getUserLocation():Promise<[number,number]>{
+   return new Promise((resolve,reject)=>{
+    debugger
+    navigator.geolocation.getCurrentPosition(
+      ({coords})=>{
+        
+      this.userLocation = [coords.latitude,coords.longitude]
+      console.log(this.userLocation)
+      resolve(this.userLocation)
+    }),reject((err: any)=>{
+      console.log(err)
+
+    })
+   });
+}
+}

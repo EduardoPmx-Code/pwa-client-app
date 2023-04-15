544cd19a4b6b195ff4577fd72905afb58b168ee8
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-15 04:02:16 -0400

    add config navigation required

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+if(!navigator.geolocation){
+  alert("navigation fail")
+  throw new Error('navigation fail')
+}
+
 if (environment.production) {
   enableProdMode();
 }

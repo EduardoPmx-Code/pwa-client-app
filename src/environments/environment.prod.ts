f2c634aeb60e63f8b65160ac5bf5f1e255ed5fcc
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-16 18:18:01 -0400

    set new env mapbox

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_url:"http://localhost:5000"
+  api_url:"http://localhost:5000",
+  map_box:"pk.eyJ1IjoiZWR1YXJkb3BteCIsImEiOiJjbGdpamk1ZDEwdnZ4M2VxNnR0OGVzYXdjIn0.WHICHEmbpRWL8012GAiq2Q"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api_url:"http://localhost:5000"
+  api_url:"http://localhost:5000",
+  map_box:"pk.eyJ1IjoiZWR1YXJkb3BteCIsImEiOiJjbGdpamk1ZDEwdnZ4M2VxNnR0OGVzYXdjIn0.WHICHEmbpRWL8012GAiq2Q"
 };
 
 /*

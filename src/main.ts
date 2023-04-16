c1c14d26009d66065aecd27f59ff4cbf33e3bbeb
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-16 18:17:41 -0400

    set config  maxbox-gl

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+import mapboxgl  from 'mapbox-gl';
 
 if(!navigator.geolocation){
   alert("navigation fail")
   throw new Error('navigation fail')
 }
+
+
+mapboxgl.accessToken = environment.map_box;
 
 if (environment.production) {
   enableProdMode();

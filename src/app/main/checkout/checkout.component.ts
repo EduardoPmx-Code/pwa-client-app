f8e4ed1bf9b7a80acd2da504e066d7be9cf4feae
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-16 18:20:15 -0400

    set config basic checkout

--- src/app/main/checkout/checkout.component.ts
+++ src/app/main/checkout/checkout.component.ts
@@ -1,58 +1,70 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import  {Map, Popup, Marker}  from 'mapbox-gl';
 import { User } from 'src/app/core/interfaces/user.model';
+import { GeolocationService } from 'src/app/core/services/geolocation.service';
 import { SessionService } from 'src/app/core/services/session.service';
+import { ShoppingCartService } from 'src/app/core/services/shopping-cart.service';
 import { UserService } from 'src/app/core/services/user.service';
-declare var paypal:any;
+
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent implements OnInit {
-  @ViewChild('paypal', { static: true })
-  paypalElement!: ElementRef;
-  product = {
-    price: 777.77,
-    description: 'used couch, decent condition',
-    img: 'assets/couch.jpg'
-  };
-  paidFor = false;
+export class CheckoutComponent implements OnInit, AfterViewInit {
+  @ViewChild("mapDiv")mapDivElement!:ElementRef
   id:any
   address:any
   paypalPay = false;
-  constructor(private userService:UserService,private sesionService: SessionService) { }
+  testForm:FormGroup
+  cartProducts:any
+  lengthProductsByCart:number = 0
+  user:any
+  geoLocation:[number,number]|undefined;
+  totalPrice!:number
+  constructor(private userService:UserService,
+    private sesionService: SessionService,
+    private cartService:ShoppingCartService,
+    private geolocationService:GeolocationService) { 
+      this.testForm = new FormGroup({
+      testValue: new FormControl(''),
+  });
+  }
+  ngAfterViewInit(): void {
+
+    if(!this.geolocationService.userLocation)throw new Error("map not found")
+
+      const map = new Map({
+        container: this.mapDivElement.nativeElement,
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center:[-62.7215398,8.2849173], //this.geolocationService.userLocation,
+        zoom:14
+      });
+      const popup = new Popup().setHTML("<h6>estas aqui</h6>");
+      new Marker({
+        color:"red"
+      }).setLngLat([-62.7215398,8.2849173]).setPopup(popup).addTo(map)
+    
+   
+  }
 
   ngOnInit(): void {
+   this.totalPrice = this.cartService.getTotalPrice()
+    this.geoLocation = this.geolocationService.userLocation
+    console.log(this.geoLocation)
    this.id =SessionService.getUser()
    console.log(this.id)
     this.userService.getUserById(this.id._id).subscribe((data)=>{
     this.address= data.address
+    this.user = data
+    console.log(this.user)
     })
-    paypal
-    .Buttons({
-      createOrder: (data:any, actions:any) => {
-        return actions.order.create({
-          purchase_units: [
-            {
-              description: this.product.description,
-              amount: {
-                currency_code: 'USD',
-                value: this.product.price
-              }
-            }
-          ]
-        });
-      },
-      onApprove: async (data:any, actions:any) => {
-        const order = await actions.order.capture();
-        this.paidFor = true;
-        console.log(order);
-      },
-      onError: (err: any) => {
-        console.log(err);
-      }
+    this.cartService.getCart().subscribe((data)=>{
+      this.cartProducts = data
     })
-    .render(this.paypalElement.nativeElement);
+    this.lengthProductsByCart = this.cartService.cartlength 
+    
 }
 checkPayment(){
   if (this.paypalPay === true) {

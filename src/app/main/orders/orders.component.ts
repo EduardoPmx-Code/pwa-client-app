ef65e1049bb9be9d8ee20664973d75cc37acd3d3
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-18 03:14:24 -0400

    call my orders

--- src/app/main/orders/orders.component.ts
+++ src/app/main/orders/orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderUserService } from 'src/app/core/services/order-user.service';
+import { SessionService } from 'src/app/core/services/session.service';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent implements OnInit {
+  id:any
+  orders!:Array<any>
 
-  constructor() { }
+  constructor(
+    private orderService:OrderUserService,
+    private Session:SessionService) {
+   }
 
   ngOnInit(): void {
+    
+   
+   this.id =SessionService.getUser()
+   console.log(this.id)
+    this.orderService.getAllByIdOrders(this.id._id).subscribe(data=>{
+      console.log(data)
+      this.orders = data
+    })
   }
 
 }

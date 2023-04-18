4a518a8f2258b1c517d6e3c8713ac08078fea86e
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-18 03:09:32 -0400

    init geolocation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GeolocationService } from '../core/services/geolocation.service';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private geolocationService:GeolocationService) {
+   }
 
   ngOnInit(): void {
+    this.geolocationService.userLocation
   }
 
 }

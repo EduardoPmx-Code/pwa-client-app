f214f102c9f1ceb6849aebf4f15834a8bacaa34f
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-18 03:14:39 -0400

    call order by id

--- src/app/main/order/order.component.ts
+++ src/app/main/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { OrderUserService } from 'src/app/core/services/order-user.service';
 
 @Component({
   selector: 'app-order',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.scss']
 })
 export class OrderComponent implements OnInit {
+  paramsId:any
+  $route!:Subscription
 
-  constructor() { }
+  constructor(private activateRoute:ActivatedRoute,private orderService:OrderUserService,) { }
 
   ngOnInit(): void {
+    this.$route = this.activateRoute.params.subscribe(
+      (data)=>{
+        this.paramsId = data
+        console.log( this.paramsId.id)
+        this.orderService.getOrderById(this.paramsId.id).subscribe(data=>{
+          console.log(data)
+        })
+      }
+    )
   }
 
 }

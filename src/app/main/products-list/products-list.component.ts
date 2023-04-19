a33cc41e19c10c3cf4b86f88596f053836b70c0f
Eduardopmx <eduardo.pimentel@totalabogados.cl>
2023-04-19 18:21:53 -0400

    add pagination 10 products page 1

--- src/app/main/products-list/products-list.component.ts
+++ src/app/main/products-list/products-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute, ParamMap, Route, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ProductsService } from 'src/app/core/services/products.service';
 
@@ -9,19 +9,19 @@ import { ProductsService } from 'src/app/core/services/products.service';
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
-export class ProductsListComponent implements OnInit, OnDestroy, OnChanges{
-  productsList:Array<any> 
+export class ProductsListComponent implements OnInit, OnDestroy{
+  productsList:any
   $products:Subscription | undefined;
   $searchProducts:Subscription | undefined;
+  page = 1;
+  limit = 10;
   $searchByCategory:Subscription | undefined;
   $route! :Subscription ;
-  search: any;
+  search!: any
 
-  constructor(private productsServices:ProductsService, private activatedRoute: ActivatedRoute,) {this.productsList=[] }
+  constructor(private productsServices:ProductsService, private activatedRoute: ActivatedRoute,private route:Router) {this.productsList=[] }
 
-  ngOnChanges(changes: SimpleChanges): void {
-   
-  }
+
   ngOnDestroy(): void {
     this.$products?.unsubscribe()
     this.$searchByCategory?.unsubscribe()
@@ -35,28 +35,19 @@ export class ProductsListComponent implements OnInit, OnDestroy, OnChanges{
       console.log(data)
       let res = data.get("search")?.split(':')
       console.log(res)
+      
       if(res){
         if(res.length === 1 && res[0] === "."){
-          this.initProductsList()
+          this.initProductsList(this.page,this.limit)
+
         }
         if(res.length === 2){
+          this.search = res[1]
           if (res[0] === "search" || "category" ){
             if(res[0] === "search" ){
-              this.$searchProducts = this.productsServices.searchproducts(res[1]).subscribe((data)=>{
-                console.log(data)
-                if(data.length !== 0){
-                  this.productsList = data
-                }else
-                this.productsList =[]
-          })
+             this. searchProducts(res[1], this.page , this.limit)
           }if(res[0] === "category" ){
-            this.$searchByCategory = this.productsServices.getProductsByCategory(res[1]).subscribe((data)=>{
-            console.log(data)
-            if(data.length !== 0){
-              this.productsList = data
-            }else
-            this.productsList =[]
-           })
+           this.searchByCategory(res[1], this.page , this.limit)
           }
           }else
           return
@@ -67,15 +58,94 @@ export class ProductsListComponent implements OnInit, OnDestroy, OnChanges{
     })
     }
   }
+  searchProducts(search:string,page:number,limit:number){
+    this.$searchProducts = this.productsServices.searchproducts(search, page , limit).subscribe((data)=>{
+      console.log(data)
+      this.productsList = data
+})
+}
+searchByCategory(category:string,page:number,limit:number){
+  this.$searchByCategory = this.productsServices.getProductsByCategory(category,page,limit).subscribe((data)=>{
+    console.log(data)
+    if(data.length !== 0){
+      this.productsList = data
+    }else
+    this.productsList =[]
+   })
+}
   
-  initProductsList(){
-    this.$products = this.productsServices.getAllProducts().subscribe(
+  initProductsList(page:number,limit:number){
+    this.$products = this.productsServices.getAllProducts(page,limit).subscribe(
        (data)=>{
          this.productsList= data
          console.log(this.productsList)
        }
      )
    }
+   loadMore(): void {
+    this.page++;
+    if(this.productsList.totalPages>= this.page){
+      if(this.activatedRoute.snapshot.queryParams['search'] !== null){
+        this.$route= this.activatedRoute.paramMap.subscribe(data=>{
+          console.log(data)
+          let res = data.get("search")?.split(':')
+          console.log(res)
+          
+          if(res){
+            if(res.length === 1 && res[0] === "."){
+              this.initProductsList(this.page,this.limit)
+            }
+            if(res.length === 2){
+              this.search = res[1]
+              if (res[0] === "search" || "category" ){
+                if(res[0] === "search" ){
+                 this. searchProducts(res[1], this.page , this.limit)
+              }if(res[0] === "category" ){
+               this.searchByCategory(res[1], this.page , this.limit)
+              }
+              }else
+              return
+            }
+          }else
+            return
+          
+        })
+        }
+  }
  
-   
+  }
+  loadPageold(){
+    this.page = this.page -1;
+    if(this.productsList.totalPages > 1 && this.productsList.currentPage >=1){
+      if(this.activatedRoute.snapshot.queryParams['search'] !== null){
+        this.$route= this.activatedRoute.paramMap.subscribe(data=>{
+          console.log(data)
+          let res = data.get("search")?.split(':')
+          console.log(res)
+          
+          if(res){
+            if(res.length === 1 && res[0] === "."){
+              this.initProductsList(this.page  , this.limit)
+            }
+            if(res.length === 2){
+              this.search = res[1]
+              if (res[0] === "search" || "category" ){
+                if(res[0] === "search" ){
+                 this. searchProducts(res[1], this.page  , this.limit)
+              }if(res[0] === "category" ){
+               this.searchByCategory(res[1], this.page  , this.limit)
+              }
+              }else
+              return
+            }
+          }else
+            return
+          
+        })
+        }
+      }
+}
+  
+
+
 }
